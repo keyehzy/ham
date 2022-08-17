0cd88c70e85487d9c65f011b7281ac64f67aee8d
Matheus S. M. de Sousa <msamuel@aluno.puc-rio.br>
2022-08-17 16:33:15 -0300

    Add GeometricPath for points

--- src/ham/geometry.h
+++ src/ham/geometry.h
@@ -16,18 +16,55 @@ class RectangleGrid {
     m_ys = linspace(m_origin.y, m_origin.y + m_height, m_size);
   };
 
-  Vec2d point(std::size_t i, std::size_t j) const {
+  Vec2d operator()(std::size_t i, std::size_t j) const {
     return Vec2d{m_xs[i], m_ys[j]};
   }
 
   double width() const { return m_width; }
   double height() const { return m_height; }
+  Vec2d origin() const { return m_origin; }
   std::size_t size() const { return m_size; }
 
  private:
   double m_width;
   double m_height;
   Vec2d m_origin;
+  std::size_t m_size;
+  std::vector<double> m_xs;
+  std::vector<double> m_ys;
+};
+
+class GeometricPath {
+ public:
+  GeometricPath(const std::vector<Vec2d>& points)
+      : m_points(points), m_size(50) {
+    // We assume that the points are sorted in the order of preference of the
+    // use i.e. the user meant that the path goes P1 -> P2 -> ..., etc.
+
+    for (std::size_t i = 0; i < m_points.size() - 1; i++) {
+      Vec2d origin = m_points[i];
+      Vec2d displacement = m_points[i + 1] - m_points[i];
+
+      std::vector<double> xs =
+          linspace(origin.x, origin.x + displacement.x, m_size);
+      std::vector<double> ys =
+          linspace(origin.y, origin.y + displacement.y, m_size);
+
+      m_xs.insert(m_xs.end(), xs.begin(), xs.end());
+      m_ys.insert(m_ys.end(), ys.begin(), ys.end());
+    }
+  };
+
+  const std::vector<Vec2d>& points() const { return m_points; }
+
+  Vec2d point(std::size_t i) const { return m_points[i]; }
+
+  std::size_t size() const { return m_size * (m_points.size() - 1); }
+
+  Vec2d operator()(std::size_t i) const { return Vec2d{m_xs[i], m_ys[i]}; }
+
+ private:
+  std::vector<Vec2d> m_points;
   std::size_t m_size;
   std::vector<double> m_xs;
   std::vector<double> m_ys;
--- src/ham/vec.h
+++ src/ham/vec.h
@@ -6,15 +6,21 @@ struct Vec2 {
   T y;
 
   Vec2<T> operator+(const Vec2<T>& v) const;
+  Vec2<T> operator-(const Vec2<T>& v) const;
   T dot(const Vec2<T>& v) const;
 };
 
 template <class T>
-Vec2<T> Vec2<T>::operator+(const Vec2<T>& v) const {
+inline Vec2<T> Vec2<T>::operator+(const Vec2<T>& v) const {
   return Vec2<T>{x + v.x, y + v.y};
 }
 
 template <class T>
-T Vec2<T>::dot(const Vec2<T>& v) const {
+inline Vec2<T> Vec2<T>::operator-(const Vec2<T>& v) const {
+  return Vec2<T>{x - v.x, y - v.y};
+}
+
+template <class T>
+inline T Vec2<T>::dot(const Vec2<T>& v) const {
   return x * v.x + y * v.y;
 }
